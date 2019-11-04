1ddd38a96734e1cdb3ee095c97bdede263feb350
wangxintong123 <815400083@qq.com>
2019-11-04 19:52:51 +0800

    11.4

---
+++ build2/precache-manifest.718bf09ad01329f9477687a988361a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7ec86736cb300d2ffd698644476437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1bd34b8d68205ebb1e",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "a7855576d128b6be874b",
+    "url": "/static/css/main.89436a62.chunk.css"
+  },
+  {
+    "revision": "af1bd34b8d68205ebb1e",
+    "url": "/static/js/2.e1f6902d.chunk.js"
+  },
+  {
+    "revision": "a7855576d128b6be874b",
+    "url": "/static/js/main.2b4baf0a.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
